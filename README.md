73365b5d1ede6fa2514f616248f721cc4672cad2
Thiago Santana <thiagosan252@gmail.com>
2025-02-06 22:27:59 -0300

    feat: script e documentação para uso

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # acc-analysis-ggir
-Análise de dados brutos de acelerômetro
+
+Este repositório contém os scripts para realização de uma análise de dados brutos de acelerômetro, utilizando a biblioteca `GGIR`.
+
+
+## Primeiros Passos
+
+1. Instale o `R` e o `RStudio`.
+
+* [R](https://cran.r-project.org/)
+* [RStudio](https://posit.co/downloads/)
+
+2. Abra o RStudio e instale os pacotes necessários.
+
+* GGIR
+
+```bash
+install.packages("GGIR", dependencies = TRUE) 
+```
+
+## Utilização
+
+1. Realize o clone ou download do repositório.
+
+2. Abra o arquivo `main.R` no RStudio.
+
+ * Os diretórios de entrada e saída estão definidos no início do arquivo em `input_dir` e `output_dir`.
+
+ * Os arquivos de entrada devem ser no formato gt3x ou csv, contendo os dados brutos do acelerômetro.
+
+ * Para utilizar o sleeplog, inclua o caminho do arquivo conforme definido em `sleeplog_file`.
+
+> [!NOTE]
+> Os diretórios no script são apenas de exemplo, podendo ser alterado de acordo com seu ambiente.
+
+3. Selecione todo o conteúdo do código e clique no botão `Source`.
+
+4. Aguarde a execução do código.
+
+5. Verifique os resultados no diretório de saída definido em `output_dir`.
+
+## Links úteis
+
+* [GGIR](https://wadpac.github.io/GGIR/index.html)
+* [GGIR Parameters](https://wadpac.github.io/GGIR/articles/GGIRParameters.html)
+* [GGIR training materials](https://www.accelting.com/ggir-training-materials/)
+
+## Dúvidas
+
+Em caso de dúvidas, entre em contato com o autor do código.
---
+++ main.R
@@ -0,0 +1,83 @@
+library(GGIR) 
+# Input directory with gt3x/csv files
+input_dir <- "C:\\Data\\input"
+# Output directory to reports and summaries
+output_dir <- "C:\\Data\\output" 
+# Sleep log file
+sleeplog_file <- "C:\\Data\\sleeplog.csv"
+
+GGIR(mode=c(1,2,3,4,5),
+     #=====================
+     # General parameters
+     #=====================
+     datadir= input_dir,
+     outputdir= output_dir,
+     do.report= c(2,4,5),
+     do.imp= TRUE,
+     storefolderstruture= FALSE,
+     overwrite= TRUE,
+     #=====================
+     # Part 1:
+     #=====================
+     windowsizes = c(5,900,3600), 
+     do.cal= TRUE,
+     do.enmo= TRUE,
+     do.mad= FALSE,
+     acc.metric= "ENMO",
+     do.anglez= TRUE,
+     chunksize= 1,
+     printsummary= TRUE,
+     #=====================
+     # Part 2
+     #=====================
+     data_masking_strategy= 1,
+     hrs.del.start= 0,
+     hrs.del.end= 0,
+     maxdur= 8,
+     includedaycrit= 8,
+     M5L5res= 10,
+     winhr= c(5),
+     qlevels= NULL,
+     qwindow= c(0,24),
+     ilevels= NULL
+     iglevels= NULL, 
+     mvpathreshold= c(201.4),
+     bout.metric= 4,
+     epochvalues2csv= TRUE,
+     #=====================
+     # Part 3
+     #=====================
+     anglethreshold= 5,
+     timethreshold= 5,
+     ignorenonwear= TRUE,
+     do.part3.pdf= TRUE,
+     #=====================
+     # Part 4
+     #=====================
+     excludefirstlast= FALSE,
+     includenightcrit= 16,
+     def.noc.sleep = 1,
+     loglocation= sleeplog_file,
+     outliers.only= FALSE,
+     criterror= 4,
+     colid= 1,
+     coln1= 2,
+     do.visual= TRUE ,
+     #=====================
+     # Part 5
+     #=====================
+     excludefirstlast.part5= FALSE,
+     save_ms5rawlevels= FALSE,
+     threshold.lig= c(35.6),      threshold.mod = c(201.4),      threshold.vig = c(707),
+     boutcriter= 0.8,
+     boutcriter.in= 0.9,      boutcriter.lig= 0.8,      boutcriter.mvpa= 0.8,
+     boutdur.in= c(1,10,30), boutdur.lig= c(1,10), boutdur.mvpa = c(1),
+     includedaycrit.part5= 2/3,
+     timewindow= c('MM'),
+     #=====================
+     # Visual report
+     #=====================
+     visualreport= TRUE,
+     dofirstpage= TRUE,
+     viewingwindow= 2,
+)
